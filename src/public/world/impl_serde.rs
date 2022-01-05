031ee2dd46cbab94e7eb98102793e63096b8d40e
Anders429 <anders.stefan.evensen@gmail.com>
2022-01-04 23:35:03 -0800

    Replace archetypes HashMap with custom Archetypes table.

--- src/public/world/impl_serde.rs
+++ src/public/world/impl_serde.rs
@@ -2,6 +2,7 @@ use crate::{
     internal::{
         archetype,
         archetype::Archetype,
+        archetypes::Archetypes,
         entity_allocator::{impl_serde::SerializedEntityAllocator, EntityAllocator},
         registry::{RegistryDeserialize, RegistrySerialize},
     },
@@ -16,26 +17,6 @@ use serde::{
     Deserialize, Deserializer, Serialize, Serializer,
 };
 
-struct SerializeArchetypes<'a, R>(&'a HashMap<archetype::Identifier<R>, Archetype<R>>)
-where
-    R: RegistrySerialize;
-
-impl<R> Serialize for SerializeArchetypes<'_, R>
-where
-    R: RegistrySerialize,
-{
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        let mut seq = serializer.serialize_seq(Some(self.0.len()))?;
-        for archetype in self.0.values() {
-            seq.serialize_element(archetype)?;
-        }
-        seq.end()
-    }
-}
-
 impl<R> Serialize for World<R>
 where
     R: RegistrySerialize,
@@ -45,59 +26,9 @@ where
         S: Serializer,
     {
         let mut r#struct = serializer.serialize_struct("World", 2)?;
-        r#struct.serialize_field("archetypes", &SerializeArchetypes(&self.archetypes))?;
+        r#struct.serialize_field("archetypes", &self.archetypes)?;
         r#struct.serialize_field("entity_allocator", &self.entity_allocator)?;
         r#struct.end()
-    }
-}
-
-struct DeserializeArchetypes<'de, R>(
-    HashMap<archetype::Identifier<R>, Archetype<R>>,
-    PhantomData<&'de ()>,
-)
-where
-    R: RegistryDeserialize<'de>;
-
-impl<'de, R> Deserialize<'de> for DeserializeArchetypes<'de, R>
-where
-    R: RegistryDeserialize<'de>,
-{
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        struct DeserializeArchetypesVisitor<'de, R>
-        where
-            R: RegistryDeserialize<'de>,
-        {
-            registry: PhantomData<&'de R>,
-        }
-
-        impl<'de, R> Visitor<'de> for DeserializeArchetypesVisitor<'de, R>
-        where
-            R: RegistryDeserialize<'de>,
-        {
-            type Value = DeserializeArchetypes<'de, R>;
-
-            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-                formatter.write_str("sequence of `Archetype`s")
-            }
-
-            fn visit_seq<S>(self, mut seq: S) -> Result<Self::Value, S::Error>
-            where
-                S: SeqAccess<'de>,
-            {
-                let mut archetypes = HashMap::with_capacity(seq.size_hint().unwrap_or(0));
-                while let Some(archetype) = seq.next_element::<Archetype<R>>()? {
-                    archetypes.insert(unsafe { archetype.identifier() }, archetype);
-                }
-                Ok(DeserializeArchetypes(archetypes, PhantomData))
-            }
-        }
-
-        deserializer.deserialize_seq(DeserializeArchetypesVisitor::<'de, R> {
-            registry: PhantomData,
-        })
     }
 }
 
@@ -148,7 +79,7 @@ where
         where
             R: RegistryDeserialize<'de>,
         {
-            archetypes: HashMap<archetype::Identifier<R>, Archetype<R>>,
+            archetypes: Archetypes<R>,
             serialized_entity_allocator: SerializedEntityAllocator,
 
             lifetime: PhantomData<&'de ()>,
@@ -175,14 +106,14 @@ where
             where
                 V: SeqAccess<'de>,
             {
-                let archetypes: DeserializeArchetypes<R> = seq
+                let archetypes = seq
                     .next_element()?
                     .ok_or_else(|| de::Error::invalid_length(0, &self))?;
                 let serialized_entity_allocator = seq
                     .next_element()?
                     .ok_or_else(|| de::Error::invalid_length(1, &self))?;
                 Ok(SerializedWorld {
-                    archetypes: archetypes.0,
+                    archetypes,
                     serialized_entity_allocator,
 
                     lifetime: PhantomData,
@@ -193,7 +124,7 @@ where
             where
                 V: MapAccess<'de>,
             {
-                let mut archetypes: Option<DeserializeArchetypes<R>> = None;
+                let mut archetypes = None;
                 let mut entity_allocator = None;
                 while let Some(key) = map.next_key()? {
                     match key {
@@ -213,8 +144,7 @@ where
                 }
                 Ok(SerializedWorld {
                     archetypes: archetypes
-                        .ok_or_else(|| de::Error::missing_field("archetypes"))?
-                        .0,
+                        .ok_or_else(|| de::Error::missing_field("archetypes"))?,
                     serialized_entity_allocator: entity_allocator
                         .ok_or_else(|| de::Error::missing_field("archetypes"))?,
 
