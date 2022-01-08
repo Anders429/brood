9fe2f70111858f7dc4ec1fea77cf742f333d6250
Anders429 <anders.stefan.evensen@gmail.com>
2022-01-07 22:55:25 -0800

    Formatting.

--- src/internal/registry/sync.rs
+++ src/internal/registry/sync.rs
@@ -1,7 +1,15 @@
-use crate::{component::Component, registry::{NullRegistry, Registry}};
+use crate::{
+    component::Component,
+    registry::{NullRegistry, Registry},
+};
 
 pub trait RegistrySync: Registry {}
 
 impl RegistrySync for NullRegistry {}
 
-impl<C, R> RegistrySync for (C, R) where C: Component + Sync, R: RegistrySync {}
+impl<C, R> RegistrySync for (C, R)
+where
+    C: Component + Sync,
+    R: RegistrySync,
+{
+}
