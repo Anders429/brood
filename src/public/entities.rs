b70f6bce70e412425b07835a1153f7969613fd1b
Anders429 <anders.stefan.evensen@gmail.com>
2022-01-07 16:49:15 -0800

    Row-wise construction using entities!() macro.

--- src/public/entities.rs
+++ src/public/entities.rs
@@ -84,12 +84,16 @@ macro_rules! entities {
     (($component:expr $(,$components:expr)* $(,)?); $n:expr) => {
         unsafe {
             $crate::entities::EntitiesIter::new_unchecked(
-                ($crate::reexports::vec![$component; $n], entities!(@internal ($($components),*); $n))
+                ($crate::reexports::vec![$component; $n], entities!(@cloned ($($components),*); $n))
             )
         }
     };
-    ($($($components:expr),*),*) => {
-        // TODO
+    ($(($($components:expr),*)),* $(,)?) => {
+        unsafe {
+            $crate::entities::EntitiesIter::new_unchecked(
+                entities!(@transpose [] $(($($components),*)),*)
+            )
+        }
     };
     ((); $n:expr) => {
         unsafe {
@@ -101,10 +105,22 @@ macro_rules! entities {
             $crate::entities::EntitiesIter::new_unchecked($crate::entities::NullEntities)
         }
     };
-    (@internal ($component:expr $(,$components:expr)* $(,)?); $n:expr) => {
-        ($crate::reexports::vec![$component; $n], entities!(@internal ($($components),*); $n))
+    (@cloned ($component:expr $(,$components:expr)* $(,)?); $n:expr) => {
+        ($crate::reexports::vec![$component; $n], entities!(@cloned ($($components),*); $n))
     };
-    (@internal (); $n:expr) => {
+    (@cloned (); $n:expr) => {
+        $crate::entities::NullEntities
+    };
+    (@transpose [$([$($column:expr),*])*] $(($component:expr $(,$components:expr)*  $(,)?)),*) => {
+        entities!(@transpose [$([$($column),*])* [$($component),*]] $(($($components),*)),*)
+    };
+    (@transpose [$([$($column:expr),*])*] $(()),*) => {
+        entities!(@as_vec ($(($($column),*)),*))
+    };
+    (@as_vec (($($column:expr),*) $(,($($columns:expr),*))* $(,)?)) => {
+        ($crate::reexports::vec![$($column),*], entities!(@as_vec ($(($($columns),*)),*)))
+    };
+    (@as_vec ()) => {
         $crate::entities::NullEntities
     };
 }
