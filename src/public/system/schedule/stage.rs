8316cdf41816c595148a8a223bdd3a3c2becb24e
Anders429 <anders.stefan.evensen@gmail.com>
2022-01-21 12:54:39 -0800

    Macro to define Stages types.

--- src/public/system/schedule/stage.rs
+++ src/public/system/schedule/stage.rs
@@ -21,3 +21,25 @@ where
     P: ParSystem<'a> + Send,
     L: Stages<'a>,
 {}
+
+#[macro_export]
+macro_rules! stages {
+    // TODO: Right now everything is comma separated. It would be ideal to have system and
+    // par_system separated with another delimiter, but I don't want to deal with trying to get the
+    // macro to accept multiple delimiters in the token tree at the moment.
+    ($($tokens:tt),* $(,)?) => {
+        stages!(internal @ $crate::system::schedule::stage::Null; $($tokens,)*)
+    };
+    (internal @ $processed:ty; system, $system:ty, $($remaining:tt),* $(,)?) => {
+        stages!(internal @ ($crate::system::schedule::stage::Stage<$system, $crate::system::Null>, $processed); $($remaining,)*)
+    };
+    (internal @ $processed:ty; par_system, $par_system:ty, $($remaining:tt),* $(,)?) => {
+        stages!(internal @ ($crate::system::schedule::stage::Stage<$crate::system::Null, $par_system>, $processed); $($remaining,)*)
+    };
+    (internal @ $processed:ty; flush, $($remaining:tt),* $(,)?) => {
+        stages!(internal @ ($crate::system::schedule::stage::Stage<$crate::system::Null, $crate::system::Null>, $processed); $($remaining,)*)
+    };
+    (internal @ $processed:ty; $($remaining:tt),* $(,)?) => {
+        $processed
+    }
+}
