5eda4d9f3f21070bc1f41034b2c31d84a4102700
Anders429 <anders.stefan.evensen@gmail.com>
2022-01-23 17:22:04 -0800

    Declare macro to define Null structs.

--- src/entities/mod.rs
+++ src/entities/mod.rs
@@ -1,53 +1,10 @@
 mod seal;
 
-use crate::component::Component;
+use crate::{component::Component, hlist::define_null};
 use alloc::vec::Vec;
 use seal::Seal;
 
-#[derive(Clone, Copy, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
-pub struct Null;
-
-#[cfg(feature = "serde")]
-mod impl_serde {
-    use super::Null;
-    use core::fmt;
-    use serde::{de, de::Visitor, Deserialize, Deserializer, Serialize, Serializer};
-
-    impl Serialize for Null {
-        fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-        where
-            S: Serializer,
-        {
-            serializer.serialize_unit_struct("Null")
-        }
-    }
-
-    impl<'de> Deserialize<'de> for Null {
-        fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-        where
-            D: Deserializer<'de>,
-        {
-            struct NullVisitor;
-
-            impl<'de> Visitor<'de> for NullVisitor {
-                type Value = Null;
-
-                fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-                    formatter.write_str("struct Null")
-                }
-
-                fn visit_unit<E>(self) -> Result<Self::Value, E>
-                where
-                    E: de::Error,
-                {
-                    Ok(Null)
-                }
-            }
-
-            deserializer.deserialize_unit_struct("Null", NullVisitor)
-        }
-    }
-}
+define_null!();
 
 pub trait Entities: Seal {}
 
--- src/registry/mod.rs
+++ src/registry/mod.rs
@@ -13,53 +13,10 @@ pub(crate) use eq::{RegistryEq, RegistryPartialEq};
 pub(crate) use send::RegistrySend;
 pub(crate) use sync::RegistrySync;
 
-use crate::component::Component;
+use crate::{component::Component, hlist::define_null};
 use seal::Seal;
 
-#[derive(Clone, Copy, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
-pub struct Null;
-
-#[cfg(feature = "serde")]
-mod impl_serde {
-    use super::Null;
-    use core::fmt;
-    use serde::{de, de::Visitor, Deserialize, Deserializer, Serialize, Serializer};
-
-    impl Serialize for Null {
-        fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-        where
-            S: Serializer,
-        {
-            serializer.serialize_unit_struct("Null")
-        }
-    }
-
-    impl<'de> Deserialize<'de> for Null {
-        fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-        where
-            D: Deserializer<'de>,
-        {
-            struct NullVisitor;
-
-            impl<'de> Visitor<'de> for NullVisitor {
-                type Value = Null;
-
-                fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-                    formatter.write_str("struct Null")
-                }
-
-                fn visit_unit<E>(self) -> Result<Self::Value, E>
-                where
-                    E: de::Error,
-                {
-                    Ok(Null)
-                }
-            }
-
-            deserializer.deserialize_unit_struct("Null", NullVisitor)
-        }
-    }
-}
+define_null!();
 
 pub trait Registry: Seal {}
 
