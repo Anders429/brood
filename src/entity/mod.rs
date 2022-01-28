0a807176065a76307e95062bf123d847df2252d5
Anders429 <anders.stefan.evensen@gmail.com>
2022-01-28 09:17:52 -0800

    Documentation for entity macro.

--- src/entity/mod.rs
+++ src/entity/mod.rs
@@ -23,6 +23,23 @@ where
 {
 }
 
+/// Creates an entity from the provided components.
+///
+/// This macro allows an enity to be defined without needing to manually create a heterogeneous
+/// list of components. Given an arbitrary number of components, this macro will arrange them into
+/// nested tuples forming a heterogeneous list.
+///
+/// # Example
+/// ``` rust
+/// use brood::entity;
+///
+/// // Define components `Foo` and `Bar`.
+/// struct Foo(u16);
+/// struct Bar(f32);
+///
+/// // Define an entity containing an instance of `Foo` and `Bar`.
+/// let my_entity = entity!(Foo(42), Bar(1.5));
+/// ```
 #[macro_export]
 macro_rules! entity {
     ($component:expr $(,$components:expr)* $(,)?) => {
