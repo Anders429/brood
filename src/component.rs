36742f9044e0491a3d4e2a2b3805f61c9e15ccf6
Anders429 <anders.stefan.evensen@gmail.com>
2022-01-27 23:45:10 -0800

    Module-level documentation for component module.

--- src/component.rs
+++ src/component.rs
@@ -1,3 +1,29 @@
+//! Types defining a single aspect of an entity.
+//!
+//! Components are defined as any type that implements the [`Component`] trait. This trait is
+//! defined automatically for any type that can be a component (which is any type that implements
+//! the [`Any`] trait), so users will be unable to implement it manually.
+//!
+//! A set of unique components forms an entity. A unique component is a component with a unique
+//! type, meaning entities cannot be created using the same component type multiple times. 
+//! Therefore, the
+//! [newtype idiom](https://doc.rust-lang.org/rust-by-example/generics/new_types.html) is useful
+//! when defining component types. For example, suppose we are defining an entity made up of two
+//! components, health and strength, both of which are a [`u32`] internally. These components would
+//! be defined as newtype structs as follows:
+//!
+//! ``` rust
+//! use brood::entity;
+//!
+//! struct Health(u32);
+//!
+//! struct Strength(u32);
+//!
+//! let my_entity = entity!(Health(10), Strength(5));
+//! ```
+//!
+//! [`Any`]: core::any::Any
+
 use core::any::Any;
 
 pub trait Component: Any {}
