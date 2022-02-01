6994d3affc875bc6b6ec74f6053bd9f56202e396
Anders429 <anders.stefan.evensen@gmail.com>
2022-01-31 19:34:45 -0800

    Don't document stages macro at crate level.

--- src/system/schedule/stage/mod.rs
+++ src/system/schedule/stage/mod.rs
@@ -1,6 +1,6 @@
 mod seal;
 
-use crate::{hlist::define_null, system::{schedule::task::Task, ParSystem, System}};
+use crate::{doc, hlist::define_null, system::{schedule::task::Task, ParSystem, System}};
 use seal::Seal;
 
 pub enum Stage<S, P> {
@@ -23,21 +23,23 @@ where
 {
 }
 
+doc::non_root_macro! {
 #[macro_export]
-macro_rules! stages {
-    ($($idents:tt $(: $systems:tt)?),* $(,)?) => {
-        stages!(internal @ $crate::system::schedule::stage::Null; $($idents $(: $systems)?,)*)
-    };
-    (internal @ $processed:ty; system: $system:ty, $($idents:tt $(: $systems:tt)?),* $(,)?) => {
-        stages!(internal @ ($crate::system::schedule::stage::Stage<$system, $crate::system::Null>, $processed); $($idents $(: $systems)?,)*)
-    };
-    (internal @ $processed:ty; par_system: $par_system:ty, $($idents:tt $(: $systems:tt)?),* $(,)?) => {
-        stages!(internal @ ($crate::system::schedule::stage::Stage<$crate::system::Null, $par_system>, $processed); $($idents $(: $systems)?,)*)
-    };
-    (internal @ $processed:ty; flush, $($idents:tt $(: $systems:tt)?),* $(,)?) => {
-        stages!(internal @ ($crate::system::schedule::stage::Stage<$crate::system::Null, $crate::system::Null>, $processed); $($idents $(: $systems)?,)*)
-    };
-    (internal @ $processed:ty; $($idents:tt $(: $systems:tt)?),* $(,)?) => {
-        $processed
-    };
+    macro_rules! stages {
+        ($($idents:tt $(: $systems:tt)?),* $(,)?) => (
+            stages!(internal @ $crate::system::schedule::stage::Null; $($idents $(: $systems)?,)*)
+        );
+        (internal @ $processed:ty; system: $system:ty, $($idents:tt $(: $systems:tt)?),* $(,)?) => (
+            stages!(internal @ ($crate::system::schedule::stage::Stage<$system, $crate::system::Null>, $processed); $($idents $(: $systems)?,)*)
+        );
+        (internal @ $processed:ty; par_system: $par_system:ty, $($idents:tt $(: $systems:tt)?),* $(,)?) => (
+            stages!(internal @ ($crate::system::schedule::stage::Stage<$crate::system::Null, $par_system>, $processed); $($idents $(: $systems)?,)*)
+        );
+        (internal @ $processed:ty; flush, $($idents:tt $(: $systems:tt)?),* $(,)?) => (
+            stages!(internal @ ($crate::system::schedule::stage::Stage<$crate::system::Null, $crate::system::Null>, $processed); $($idents $(: $systems)?,)*)
+        );
+        (internal @ $processed:ty; $($idents:tt $(: $systems:tt)?),* $(,)?) => (
+            $processed
+        );
+}
 }
