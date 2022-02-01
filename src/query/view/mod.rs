d247fc35552e6200685c46b4cc2e6f4e21ac5461
Anders429 <anders.stefan.evensen@gmail.com>
2022-01-31 17:44:59 -0800

    Documentation for views macro.

--- src/query/view/mod.rs
+++ src/query/view/mod.rs
@@ -33,6 +33,31 @@ where
 {
 }
 
+/// Creates a set of [`View`]s over components.
+///
+/// These views can be used to [`query`] the components stored within a [`World`]. They can also be
+/// used when defining [`System`]s to be run over components stored in a [`World`].
+///
+/// See the documentation for [`View`] to learn more about what kinds of `View`s can be created.
+///
+/// # Example
+/// ``` rust
+/// use brood::query::views;
+///
+/// // Define components.
+/// struct Foo(u32);
+/// struct Bar(bool);
+///
+/// type Views<'a> = views!(&'a mut Foo, &'a Bar);
+/// ```
+///
+/// Note that the lifetime `'a` can often be omitted when [`query`]ing a [`World`], but is required
+/// when defining a [`System`].
+///
+/// [`query`]: crate::world::World::query()
+/// [`System`]: crate::system::System
+/// [`View`]: crate::query::view::View
+/// [`World`]: crate::world::World
 #[macro_export]
 macro_rules! views {
     ($view:ty $(,$views:ty)* $(,)?) => {
