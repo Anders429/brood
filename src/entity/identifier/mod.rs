15ec4aeb1d0a835c3c2ab69d3e3f48d4c770e959
Anders429 <anders.stefan.evensen@gmail.com>
2022-02-02 01:44:43 -0800

    Documentation for entity::Identifier.

--- src/entity/identifier/mod.rs
+++ src/entity/identifier/mod.rs
@@ -2,6 +2,31 @@
 #[cfg_attr(doc_cfg, doc(cfg(feature = "serde")))]
 mod impl_serde;
 
+/// A unique identifier for an entity.
+///
+/// An `Identifier` can be used to reference an entity that is stored within a [`World`].
+/// `Identifier`s are normally obtained when inserting an entity into a `World`, although they can
+/// also be obtained through a [`query`] on a `World` by providing `Identifier` as a [`View`].
+///
+/// # Example
+/// ``` rust
+/// use brood::{entity, registry, World};
+///
+/// // Define components.
+/// struct Foo(usize);
+/// struct Bar(bool);
+///
+/// type Registry = registry!(Foo, Bar);
+///
+/// let mut world = World::<Registry>::new();
+///
+/// // An identifier is returned on insertion.
+/// let entity_identifier = world.push(entity!(Foo(42), Bar(false)));
+/// ```
+///
+/// [`query`]: crate::world::World::query()
+/// [`View`]: crate::query::view::View
+/// [`World`]: crate::world::World
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 pub struct Identifier {
     pub(crate) index: usize,
