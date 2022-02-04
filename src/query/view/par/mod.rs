bd3bd78103d763bf04eddb7a81262253c83f238c
Anders429 <anders.stefan.evensen@gmail.com>
2022-02-04 09:28:44 -0800

    Documentation for query::view::ParView.

--- src/query/view/par/mod.rs
+++ src/query/view/par/mod.rs
@@ -7,6 +7,30 @@ use crate::{
 };
 use seal::{ParViewSeal, ParViewsSeal};
 
+/// A parallel view over a single aspect of an entity.
+///
+/// The main difference between this trait and the standard [`View`] trait is that this view can be
+/// shared between threads, allowing it to be used within parallel iteration in either a
+/// [`ParSystem`] or a [`par_query`].
+///
+/// All types that implement `View` also implement `ParView`, so long as any [`Component`] `C` they
+/// view is [`Send`] when viewed mutably or [`Sync`] when viewed immutably.
+///
+/// # Example
+/// ``` rust
+/// // Define a component.
+/// struct Foo(usize);
+///
+/// // Define a view over that component.
+/// type FooView<'a> = &'a Foo;
+/// ```
+///
+/// Because the `Component` viewed in the above example implements `Sync`, the view created above
+/// implements `ParView`.
+///
+/// [`ParSystem`]: crate::system::ParSystem
+/// [`par_query`]: crate::world::World::par_query()
+/// [`View`]: crate::query::view::View
 #[cfg_attr(doc_cfg, doc(cfg(feature = "parallel")))]
 pub trait ParView<'a>: Filter + ParViewSeal<'a> {}
 
