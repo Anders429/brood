acb19b4b7364b266cc3d45c5dd5118fc58f422df
Anders429 <anders.stefan.evensen@gmail.com>
2022-02-04 21:17:31 -0800

    Documentation for query::result module.

--- src/query/result/mod.rs
+++ src/query/result/mod.rs
@@ -1,3 +1,44 @@
+//! Results of queries.
+//!
+//! The primary way to interact with entities stored within a [`World`] is to query their
+//! [`Component`]s using [`Views`] and [`Filter`]s. This module handles interaction with the
+//! results of those queries.
+//!
+//! As `Component`s should be allowed to be queried arbitrarily, with any amount of `Component`s
+//! and any amount of `Filter`s being requested at once, the returned results of queries must be
+//! just as flexible. Therefore, the returned results are in the form of heterogeneous lists. In
+//! order to unpack these values into usable identifiers, a [`result!`] macro is provided to remove
+//! the unpleasant boilerplate.
+//!
+//! # Example
+//! The following example queries a `World` for all entities containing two `Component`s, giving
+//! `View`s over both `Component`s. One of these `View`s is mutable, allowing the component to be
+//! modified during iteration.
+//!
+//! ``` rust
+//! use brood::{entity, query::{filter, result, views}, registry, World};
+//!
+//! struct Foo(u32);
+//! struct Bar(bool);
+//!
+//! type Registry = registry!(Foo, Bar);
+//!
+//! let mut world = World::<Registry>::new();
+//! world.push(entity!(Foo(42), Bar(true)));
+//!
+//! for result!(foo, bar) in world.query::<views!(&mut Foo, &Bar), filter::None>() {
+//!     if bar.0 {
+//!         foo.0 += 1;
+//!     }
+//! }
+//! ```
+//!
+//! [`Component`]: crate::component::Component
+//! [`Filter`]: crate::query::filter::Filter
+//! [`result!`]: crate::query::result!
+//! [`Views`]: crate::query::view::Views
+//! [`World`]: crate::world::World
+
 mod iter;
 #[cfg(feature = "parallel")]
 mod par_iter;
