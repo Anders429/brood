7e0429e41cfba0a03ddc4570b7d46ab88c9a0923
Anders429 <anders.stefan.evensen@gmail.com>
2022-02-04 20:51:11 -0800

    Documentation for query::view::ParViews trait.

--- src/query/view/par/mod.rs
+++ src/query/view/par/mod.rs
@@ -28,6 +28,7 @@ use seal::{ParViewSeal, ParViewsSeal};
 /// Because the `Component` viewed in the above example implements `Sync`, the view created above
 /// implements `ParView`.
 ///
+/// [`Component`]: crate::component::Component
 /// [`ParSystem`]: crate::system::ParSystem
 /// [`par_query`]: crate::world::World::par_query()
 /// [`View`]: crate::query::view::View
@@ -44,6 +45,35 @@ impl<'a, C> ParView<'a> for Option<&mut C> where C: Component + Send {}
 
 impl<'a> ParView<'a> for entity::Identifier {}
 
+/// A heterogeneous list of [`ParView`]s.
+///
+/// The main difference between this trait and the standard [`Views`] trait is that these views can
+/// be shared between threads, allowing them to be used within parallel iteration in either a
+/// [`ParSystem`] or a [`par_query`].
+///
+/// All types that implement `Views` also implement `ParViews`, so long as any [`Component`]s `C`
+/// they view are [`Send`] when viewed mutably or [`Sync`] when viewed immutably.
+///
+/// # Example
+/// ``` rust
+/// use brood::query::views;
+///
+/// // Define components.
+/// struct Foo(usize);
+/// struct Bar(bool);
+///
+/// // Define views over those components.
+/// type Views<'a> = views!(&'a Foo, &'a mut Bar);
+/// ```
+///
+/// Because the `Component`s viewed above implement both [`Send`] and [`Sync`], the views created
+/// above implement `ParViews`.
+///
+/// [`Component`]: crate::component::Component
+/// [`ParSystem`]: crate::system::ParSystem
+/// [`ParView`]: crate::query::view::ParView
+/// [`par_query`]: crate::world::World::par_query()
+/// [`Views`]: crate::query::view::Views
 #[cfg_attr(doc_cfg, doc(cfg(feature = "parallel")))]
 pub trait ParViews<'a>: Filter + ParViewsSeal<'a> {}
 
