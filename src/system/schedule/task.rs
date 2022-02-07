2aad7db44c5c0e4bb7a0005d442499d2d66719d9
Anders429 <anders.stefan.evensen@gmail.com>
2022-02-06 19:50:33 -0800

    Remove unnecessary &mut.

--- src/system/schedule/task.rs
+++ src/system/schedule/task.rs
@@ -21,14 +21,14 @@ where
         match self {
             Task::Seq(system) => {
                 // Query world using system.
-                let result = unsafe { (&mut *world.0).query_unchecked::<S::Views, S::Filter>() };
+                let result = unsafe { (*world.0).query_unchecked::<S::Views, S::Filter>() };
                 // Run system using the query result.
                 system.run(result);
             }
             Task::Par(system) => {
                 // Query world using system.
                 let result =
-                    unsafe { (&mut *world.0).par_query_unchecked::<P::Views, P::Filter>() };
+                    unsafe { (*world.0).par_query_unchecked::<P::Views, P::Filter>() };
                 // Run system using the query result.
                 system.run(result);
             }
