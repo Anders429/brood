be5b12881b80370422b5a366d22b1e31d92168d2
Anders429 <anders.stefan.evensen@gmail.com>
2022-02-13 13:45:51 -0800

    Revert "Remove unnecessary query_unchecked methods."
    This reverts commit 36ca72407b3164c47916c779e248dfdbfddf0e70.

--- src/system/schedule/task.rs
+++ src/system/schedule/task.rs
@@ -21,13 +21,13 @@ where
         match self {
             Task::Seq(system) => {
                 // Query world using system.
-                let result = unsafe { (*world.0).query::<S::Views, S::Filter>() };
+                let result = unsafe { (*world.0).query_unchecked::<S::Views, S::Filter>() };
                 // Run system using the query result.
                 system.run(result);
             }
             Task::Par(system) => {
                 // Query world using system.
-                let result = unsafe { (*world.0).par_query::<P::Views, P::Filter>() };
+                let result = unsafe { (*world.0).par_query_unchecked::<P::Views, P::Filter>() };
                 // Run system using the query result.
                 system.run(result);
             }
