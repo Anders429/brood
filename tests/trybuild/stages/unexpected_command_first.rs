3e04a58e7240c59153c6d847475cabd4f267c988
Anders429 <anders.stefan.evensen@gmail.com>
2022-02-15 18:41:30 -0800

    Trybuild tests for more unexpected command variations.

---
+++ tests/trybuild/stages/unexpected_command_first.rs
@@ -0,0 +1,8 @@
+use brood::system::schedule::stages;
+
+type Stages = stages!{
+    unexpected,
+    flush,
+};
+
+fn main() {}
---
+++ tests/trybuild/stages/unexpected_command_middle.rs
@@ -0,0 +1,9 @@
+use brood::system::schedule::stages;
+
+type Stages = stages!{
+    flush,
+    unexpected,
+    flush,
+};
+
+fn main() {}
