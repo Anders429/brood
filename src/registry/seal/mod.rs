6866cada34c627f0af473ee78e6b1a84fa6ecc7f
Anders429 <anders.stefan.evensen@gmail.com>
2022-03-13 15:15:26 -0700

    Assert Registry does not have duplicate components.

--- src/registry/seal/mod.rs
+++ src/registry/seal/mod.rs
@@ -1,11 +1,13 @@
+mod assertions;
 mod length;
 mod storage;
 
 use crate::{component::Component, registry::Null};
+use assertions::Assertions;
 use length::Length;
 use storage::Storage;
 
-pub trait Seal: Length + Storage {}
+pub trait Seal: Assertions + Length + Storage {}
 
 impl Seal for Null {}
 
