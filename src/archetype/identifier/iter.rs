7f1bcb3352b1b9dc5b3d8a977b6525ba9fee5a68
Anders429 <anders.stefan.evensen@gmail.com>
2022-03-22 22:26:07 -0700

    Safety comments for archetype::iterator::Iter.

--- src/archetype/identifier/iter.rs
+++ src/archetype/identifier/iter.rs
@@ -1,15 +1,42 @@
 use crate::registry::Registry;
 use core::marker::PhantomData;
 
+/// An iterator over the bits of an [`Identifier`].
+///
+/// This iterator is guaranteed to return exactly `(R::LEN + 7) / 8` boolean values indicating
+/// the components within `R` that are identified.
+///
+/// [`Identifier`]: crate::archetype::identifier::Identifier
 pub struct Iter<R>
 where
     R: Registry,
 {
+    /// The [`Registry`] defining the set of valid values of this identifier.
+    ///
+    /// Each identifier must exist within a set defined by a `Registry`. This defines a space over
+    /// which each identifier can uniquely define a set of components. Each bit within the
+    /// identifier corresponds with a component in the registry.
+    ///
+    /// [`Registry`]: crate::registry::Registry
     registry: PhantomData<R>,
 
+    /// A pointer to the allocated bits.
+    ///
+    /// Note that this allocation is not owned by this struct. It is owned by an [`Identifier`] and
+    /// is invariantly guaranteed to outlive this struct.
+    ///
+    /// As iteration progresses, this pointer will move along to point to the current byte.
+    ///
+    /// [`Identifier`]: crate::archetype::identifier::Identifier
     pointer: *const u8,
 
+    /// The current byte being iterated over.
+    ///
+    /// This byte just includes the remaining bits of the current value.
     current: u8,
+    /// The current bit position.
+    ///
+    /// If this value is greater than or equal to `(R::LEN + 7) / 8`, iteration has completed.
     position: usize,
 }
 
@@ -17,13 +44,26 @@ impl<R> Iter<R>
 where
     R: Registry,
 {
+    /// Create a new iterator for an [`Identifier`].
+    ///
+    /// # Safety
+    /// `pointer` must be a pointer to a valid `Identifier` allocation, and must be ensured to live
+    /// as long as the returned `Iter`.
+    ///
+    /// [`Identifier`]: crate::archetype::identifier::Identifier
     pub(super) unsafe fn new(pointer: *const u8) -> Self {
         Self {
             registry: PhantomData,
 
             pointer,
 
-            current: if R::LEN > 0 { unsafe { *pointer } } else { 0 },
+            current: if R::LEN > 0 {
+                // SAFETY: `pointer` is a valid `Identifier` allocation that is nonempty, meaning
+                // it points to at least one `u8` which is dereferenced here.
+                unsafe { *pointer }
+            } else {
+                0
+            },
             position: 0,
         }
     }
@@ -42,8 +82,16 @@ where
             let result = self.current & 1 != 0;
             self.position += 1;
             if self.position < R::LEN && self.position % 8 == 0 {
-                self.pointer = unsafe { self.pointer.add(1) };
-                self.current = unsafe { *self.pointer };
+                self.pointer =
+                    // SAFETY: The allocation pointed to is guaranteed to have at least
+                    // `(R::LEN + 7) / 8` bytes. Therefore, since `self.position` is only
+                    // incremented once on each iteration, we will only enter this block for every
+                    // eighth byte and therefore not offset past the end of the allocation.
+                    unsafe { self.pointer.add(1) };
+                self.current =
+                    // SAFETY: Due to the reasons above, `self.pointer` will always point to a
+                    // valid `u8` that can be dereferenced here without fail.
+                    unsafe { *self.pointer };
             } else {
                 self.current >>= 1;
             }
