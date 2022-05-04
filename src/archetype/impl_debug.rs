ef3474f365b39e9b5a3a4318169ab7f827f0ca8d
Anders429 <anders.stefan.evensen@gmail.com>
2022-05-04 00:38:41 -0700

    Safety comments in Archetype debug impl.

--- src/archetype/impl_debug.rs
+++ src/archetype/impl_debug.rs
@@ -19,6 +19,9 @@ where
 {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let mut debug_map = f.debug_map();
+        // SAFETY: `pointers` is guaranteed to contain the same number of values as there are
+        // components, each pointing to a valid component of type `C`. Also, `self.identifier` will
+        // yield the same number of values as there are components in `R`.
         unsafe {
             R::debug_components(&self.pointers, &mut debug_map, self.identifier.iter());
         }
@@ -53,16 +56,24 @@ where
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let mut debug_map = f.debug_map();
 
-        let entity_identifiers = ManuallyDrop::new(unsafe {
-            Vec::from_raw_parts(
-                self.entity_identifiers.0,
-                self.length,
-                self.entity_identifiers.1,
-            )
-        });
+        let entity_identifiers = ManuallyDrop::new(
+            // SAFETY: `self.identifiers`, together with `self.length`, are guaranteed to be the
+            // raw parts for a valid `Vec<entity::Identifier>`.
+            unsafe {
+                Vec::from_raw_parts(
+                    self.entity_identifiers.0,
+                    self.length,
+                    self.entity_identifiers.1,
+                )
+            },
+        );
 
         for i in 0..self.length {
             let mut component_pointers = Vec::new();
+            // SAFETY: `self.components` contains the same number of values as
+            // `self.identifier.iter()` has bits. Each tuple in `components` corresponds to a valid
+            // `Vec<C>` for each component `C` with a length of `self.length`. Therefore, `i` will
+            // be within the bounds of each of those `Vec<C>`s as well.
             unsafe {
                 R::extract_component_pointers(
                     i,
@@ -74,9 +85,12 @@ where
             debug_map.entry(
                 &i,
                 &Row::<R> {
+                    // SAFETY: `entity_identifiers` is guaranteed to be of length `self.length`.
                     identifier: *unsafe { entity_identifiers.get_unchecked(i) },
                     components: Components {
                         pointers: component_pointers,
+                        // SAFETY: This `IdentifierRef` will not outlive its referenced
+                        // `Identifier`.
                         identifier: unsafe { self.identifier.as_ref() },
                     },
                 },
