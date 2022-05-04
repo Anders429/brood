8e44c29d022533fd2f1992127f4acca5a85fc013
Anders429 <anders.stefan.evensen@gmail.com>
2022-05-04 00:08:29 -0700

    Safety comments for Task::run().

--- src/system/schedule/task.rs
+++ src/system/schedule/task.rs
@@ -21,12 +21,15 @@ where
         match self {
             Task::Seq(system) => {
                 // Query world using system.
+                // SAFETY: The `Views` checks were already done when constructing the `Schedule`.
                 let result = unsafe { (*world.0).query_unchecked::<S::Views, S::Filter>() };
                 // Run system using the query result.
                 system.run(result);
             }
             Task::Par(system) => {
                 // Query world using system.
+                // SAFETY: The `ParViews` checks were already done when constructing the
+                // `Schedule`.
                 let result = unsafe { (*world.0).par_query_unchecked::<P::Views, P::Filter>() };
                 // Run system using the query result.
                 system.run(result);
