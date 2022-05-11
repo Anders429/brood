91193d5399744847094502b3cd9ec6d973711d99
Anders429 <anders.stefan.evensen@gmail.com>
2022-05-10 23:29:07 -0700

    Safety comments for Archetype Eq impls.

--- src/archetype/impl_eq.rs
+++ src/archetype/impl_eq.rs
@@ -12,20 +12,36 @@ where
     fn eq(&self, other: &Self) -> bool {
         self.length == other.length
             && self.identifier == other.identifier
-            && ManuallyDrop::new(unsafe {
+            && ManuallyDrop::new(
+                // SAFETY: `self.entity_identifiers` is guaranteed to contain the raw parts for a
+                // valid `Vec` of size `self.length`.
+                unsafe {
                 Vec::from_raw_parts(
                     self.entity_identifiers.0,
                     self.length,
                     self.entity_identifiers.1,
                 )
-            }) == ManuallyDrop::new(unsafe {
+            }) == ManuallyDrop::new(
+                // SAFETY: `other.entity_identifiers` is guaranteed to contain the raw parts for a
+                // valid `Vec` of size `other.length`.
+                unsafe {
                 Vec::from_raw_parts(
                     other.entity_identifiers.0,
                     other.length,
                     other.entity_identifiers.1,
                 )
             })
-            && unsafe {
+            &&
+            // SAFETY: Since `self.identifier` is equal to `other.identifier`, the components Vecs
+            // will contain the same number of values as there are bits in `self.identifier`.
+            //
+            // `self.components` and `other.components` both contain raw parts for valid `Vec<C>`s
+            // for each identified component `C` of size `self.length` (since `self.length` and
+            // `other.length` are equal).
+            //
+            // `self.identifier` is generic over the same `R` upon which this function is being
+            // called.
+            unsafe {
                 R::component_eq(
                     &self.components,
                     &other.components,
