410349ab93cbe41cb4ce66f8dc91fd490bb90bf2
Anders429 <anders.stefan.evensen@gmail.com>
2022-05-16 23:12:00 -0700

    Fix trybuild test.

--- tests/trybuild.rs
+++ tests/trybuild.rs
@@ -2,8 +2,8 @@
 
 macro_rules! trybuild_test {
     ($test_name:ident) => {
-        #[test]
         #[rustversion::attr(not(nightly), ignore)]
+        #[test]
         fn $test_name() {
             trybuild::TestCases::new().compile_fail(concat!(
                 "tests/trybuild/",
