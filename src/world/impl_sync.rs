e2bc61da28b211bba878d05670038b9e2a09b31e
Anders429 <anders.stefan.evensen@gmail.com>
2022-08-08 17:43:27 -0700

    Safety comments for World Send and Sync impls.

--- src/world/impl_sync.rs
+++ src/world/impl_sync.rs
@@ -1,3 +1,5 @@
 use crate::{registry::RegistrySync, world::World};
 
+// SAFETY: This type is safe to share between multiple threads as you can't mutate it without a
+// &mut reference.
 unsafe impl<R> Sync for World<R> where R: RegistrySync {}
