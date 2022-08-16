5fd6dfcd16a1e484038c92ba30567faa597768da
Anders429 <anders.stefan.evensen@gmail.com>
2022-08-16 16:09:42 -0700

    Test hlist::define_null.

--- src/hlist.rs
+++ src/hlist.rs
@@ -55,3 +55,48 @@ macro_rules! define_null {
 }
 
 pub(crate) use define_null;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[cfg(feature = "serde")]
+    use serde_test::{assert_de_tokens, assert_de_tokens_error, assert_tokens, Token};
+
+    define_null!();
+
+    #[cfg(feature = "serde")]
+    #[test]
+    fn serialize_deserialize() {
+        assert_tokens(
+            &Null,
+            &[
+                Token::UnitStruct {
+                    name: "Null",
+                },
+            ],
+        );
+    }
+
+    #[cfg(feature = "serde")]
+    #[test]
+    fn deserialize_from_unit() {
+        assert_de_tokens(
+            &Null,
+            &[
+                Token::Unit,
+            ],
+        );
+    }
+
+    #[cfg(feature = "serde")]
+    #[test]
+    fn deserialize_from_invalid_type() {
+        assert_de_tokens_error::<Null>(
+            &[
+                Token::U32(42),
+            ],
+            "invalid type: integer `42`, expected struct Null"
+        );
+    }
+}
+
