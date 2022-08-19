f99a9861bac685492d555c074501c8de1bcd89a4
Anders429 <anders.stefan.evensen@gmail.com>
2022-08-18 23:58:43 -0700

    Test claims.

--- src/query/claim.rs
+++ src/query/claim.rs
@@ -87,3 +87,109 @@ where
         W::claim(mutable_claims, immutable_claims);
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use hashbrown::HashSet;
+
+    struct A;
+    struct B;
+
+    #[test]
+    fn claim_ref() {
+        let mut mutable_claims = HashSet::with_hasher(FnvBuildHasher::default());
+        let mut immutable_claims = HashSet::with_hasher(FnvBuildHasher::default());
+
+        <&A>::claim(&mut mutable_claims, &mut immutable_claims);
+
+        let expected_mutable_claims = HashSet::with_hasher(FnvBuildHasher::default());
+        let mut expected_immutable_claims = HashSet::with_hasher(FnvBuildHasher::default());
+        expected_immutable_claims.insert(TypeId::of::<A>());
+        assert_eq!(mutable_claims, expected_mutable_claims);
+        assert_eq!(immutable_claims, expected_immutable_claims);
+    }
+
+    #[test]
+    fn claim_mut_ref() {
+        let mut mutable_claims = HashSet::with_hasher(FnvBuildHasher::default());
+        let mut immutable_claims = HashSet::with_hasher(FnvBuildHasher::default());
+
+        <&mut A>::claim(&mut mutable_claims, &mut immutable_claims);
+
+        let mut expected_mutable_claims = HashSet::with_hasher(FnvBuildHasher::default());
+        let expected_immutable_claims = HashSet::with_hasher(FnvBuildHasher::default());
+        expected_mutable_claims.insert(TypeId::of::<A>());
+        assert_eq!(mutable_claims, expected_mutable_claims);
+        assert_eq!(immutable_claims, expected_immutable_claims);
+    }
+
+    #[test]
+    fn claim_option() {
+        let mut mutable_claims = HashSet::with_hasher(FnvBuildHasher::default());
+        let mut immutable_claims = HashSet::with_hasher(FnvBuildHasher::default());
+
+        <Option<&A>>::claim(&mut mutable_claims, &mut immutable_claims);
+
+        let expected_mutable_claims = HashSet::with_hasher(FnvBuildHasher::default());
+        let mut expected_immutable_claims = HashSet::with_hasher(FnvBuildHasher::default());
+        expected_immutable_claims.insert(TypeId::of::<A>());
+        assert_eq!(mutable_claims, expected_mutable_claims);
+        assert_eq!(immutable_claims, expected_immutable_claims);
+    }
+
+    #[test]
+    fn claim_mut_option() {
+        let mut mutable_claims = HashSet::with_hasher(FnvBuildHasher::default());
+        let mut immutable_claims = HashSet::with_hasher(FnvBuildHasher::default());
+
+        <Option<&mut A>>::claim(&mut mutable_claims, &mut immutable_claims);
+
+        let mut expected_mutable_claims = HashSet::with_hasher(FnvBuildHasher::default());
+        let expected_immutable_claims = HashSet::with_hasher(FnvBuildHasher::default());
+        expected_mutable_claims.insert(TypeId::of::<A>());
+        assert_eq!(mutable_claims, expected_mutable_claims);
+        assert_eq!(immutable_claims, expected_immutable_claims);
+    }
+
+    #[test]
+    fn claim_entity_identifier() {
+        let mut mutable_claims = HashSet::with_hasher(FnvBuildHasher::default());
+        let mut immutable_claims = HashSet::with_hasher(FnvBuildHasher::default());
+
+        <entity::Identifier>::claim(&mut mutable_claims, &mut immutable_claims);
+
+        let expected_mutable_claims = HashSet::with_hasher(FnvBuildHasher::default());
+        let expected_immutable_claims = HashSet::with_hasher(FnvBuildHasher::default());
+        assert_eq!(mutable_claims, expected_mutable_claims);
+        assert_eq!(immutable_claims, expected_immutable_claims);
+    }
+
+    #[test]
+    fn claim_view_null() {
+        let mut mutable_claims = HashSet::with_hasher(FnvBuildHasher::default());
+        let mut immutable_claims = HashSet::with_hasher(FnvBuildHasher::default());
+
+        <view::Null>::claim(&mut mutable_claims, &mut immutable_claims);
+
+        let expected_mutable_claims = HashSet::with_hasher(FnvBuildHasher::default());
+        let expected_immutable_claims = HashSet::with_hasher(FnvBuildHasher::default());
+        assert_eq!(mutable_claims, expected_mutable_claims);
+        assert_eq!(immutable_claims, expected_immutable_claims);
+    }
+
+    #[test]
+    fn claim_hlist() {
+        let mut mutable_claims = HashSet::with_hasher(FnvBuildHasher::default());
+        let mut immutable_claims = HashSet::with_hasher(FnvBuildHasher::default());
+
+        <(&A, (&mut B, view::Null))>::claim(&mut mutable_claims, &mut immutable_claims);
+
+        let mut expected_mutable_claims = HashSet::with_hasher(FnvBuildHasher::default());
+        let mut expected_immutable_claims = HashSet::with_hasher(FnvBuildHasher::default());
+        expected_immutable_claims.insert(TypeId::of::<A>());
+        expected_mutable_claims.insert(TypeId::of::<B>());
+        assert_eq!(mutable_claims, expected_mutable_claims);
+        assert_eq!(immutable_claims, expected_immutable_claims);
+    }
+}
