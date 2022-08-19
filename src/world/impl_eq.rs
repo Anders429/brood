dd1da82ca478931d04d322ae147276b475e43b92
Anders429 <anders.stefan.evensen@gmail.com>
2022-08-19 00:17:45 -0700

    Test world equality.

--- src/world/impl_eq.rs
+++ src/world/impl_eq.rs
@@ -16,5 +16,72 @@ impl<R> Eq for World<R> where R: RegistryEq {}
 
 #[cfg(test)]
 mod tests {
-    // TODO
+    use super::*;
+    use crate::{entity, registry};
+
+    #[derive(Debug, Eq, PartialEq)]
+    struct A(u32);
+
+    #[derive(Debug, Eq, PartialEq)]
+    struct B(char);
+
+    type Registry = registry!(A, B);
+
+    #[test]
+    fn empty_eq() {
+        assert_eq!(World::<registry!()>::new(), World::<registry!()>::new());
+    }
+
+    #[test]
+    fn with_entities_eq() {
+        let mut world_a = World::<Registry>::new();
+        let mut world_b = World::<Registry>::new();
+
+        world_a.insert(entity!(A(1), B('a')));
+        world_a.insert(entity!(A(2), B('b')));
+        world_a.insert(entity!(A(3), B('c')));
+        world_a.insert(entity!(A(4)));
+        world_a.insert(entity!(A(5)));
+        world_a.insert(entity!());
+
+        world_b.insert(entity!(A(1), B('a')));
+        world_b.insert(entity!(A(2), B('b')));
+        world_b.insert(entity!(A(3), B('c')));
+        world_b.insert(entity!(A(4)));
+        world_b.insert(entity!(A(5)));
+        world_b.insert(entity!());
+
+        assert_eq!(world_a, world_b);
+    }
+
+    #[test]
+    fn archetypes_not_equal() {
+        let mut world_a = World::<Registry>::new();
+        let mut world_b = World::<Registry>::new();
+
+        world_a.insert(entity!(A(1), B('a')));
+        world_a.insert(entity!(A(2), B('b')));
+        world_a.insert(entity!(A(3), B('c')));
+
+        world_b.insert(entity!(A(1)));
+        world_b.insert(entity!(A(2)));
+        world_b.insert(entity!(A(3)));
+
+        assert_ne!(world_a, world_b);
+    }
+
+    #[test]
+    fn allocators_not_equal() {
+        let mut world_a = World::<Registry>::new();
+        let mut world_b = World::<Registry>::new();
+
+        world_a.insert(entity!(A(1), B('a')));
+
+        let entity_identifier = world_b.insert(entity!(A(1), B('a')));
+        world_b.remove(entity_identifier);
+        world_b.insert(entity!(A(1), B('a')));
+
+        // The generational index of the entities will be different.
+        assert_ne!(world_a, world_b);
+    }
 }
