35cbc31b0f4ad6ba668299083b3b197fca0b3035
Anders429 <anders.stefan.evensen@gmail.com>
2022-08-21 23:22:30 -0700

    Add querying to entry API.

--- src/query/view/seal.rs
+++ src/query/view/seal.rs
@@ -34,6 +34,31 @@ pub trait ViewSeal<'a>: Claim {
         component_map: &HashMap<TypeId, usize, FnvBuildHasher>,
     ) -> Self::Result;
 
+    /// View a specific row of an `Archetype`.
+    /// 
+    /// # Safety
+    /// Each tuple in `columns` must contain the raw parts for a valid `Vec<C>` of size `length`
+    /// for components `C`. Each of those components `C` must have an entry in `component_map`,
+    /// paired with the correct index corresponding to that component's entry in `columns`.
+    ///
+    /// `entity_identifiers` must contain the raw parts for a valid `Vec<entity::Identifier` of
+    /// size `length`.
+    ///
+    /// `component_map` must contain an entry for every component `C` that is viewed by this
+    /// `View`, and that entry must contain the index for the column of type `C` in `columns`. Note
+    /// that it is not required for optionally viewed components to be contained in the
+    /// `component_map`.
+    /// 
+    /// `index` must be a valid index in the `Vec<entity::Identifier>` and each component column
+    /// `Vec<C>`.
+    unsafe fn view_one(
+        index: usize,
+        columns: &[(*mut u8, usize)],
+        entity_identifiers: (*mut entity::Identifier, usize),
+        length: usize,
+        component_map: &HashMap<TypeId, usize, FnvBuildHasher>,
+    ) -> <Self::Result as Iterator>::Item;
+
     fn assert_claim(buffer: &mut AssertionBuffer);
 }
 
@@ -63,6 +88,29 @@ where
             )
         }
         .iter()
+    }
+
+    unsafe fn view_one(
+        index: usize,
+        columns: &[(*mut u8, usize)],
+        _entity_identifiers: (*mut entity::Identifier, usize),
+        length: usize,
+        component_map: &HashMap<TypeId, usize, FnvBuildHasher>,
+    ) -> <Self::Result as Iterator>::Item {
+        // SAFETY: `columns` is guaranteed to contain raw parts for a valid `Vec<C>` of size
+        // `length`. Since `component_map` contains an entry for the given component `C`'s entry in
+        // `columns`, then the column obtained here can be interpreted as a slice of type `C` of
+        // size `length`. `index` is guaranteed to be a valid index into the `Vec<C>`.
+        unsafe {
+            slice::from_raw_parts::<'a, C>(
+                columns
+                    .get_unchecked(*component_map.get(&TypeId::of::<C>()).unwrap_unchecked())
+                    .0
+                    .cast::<C>(),
+                length,
+            )
+            .get_unchecked(index)
+        }
     }
 
     fn assert_claim(buffer: &mut AssertionBuffer) {
@@ -96,6 +144,29 @@ where
             )
         }
         .iter_mut()
+    }
+
+    unsafe fn view_one(
+        index: usize,
+        columns: &[(*mut u8, usize)],
+        _entity_identifiers: (*mut entity::Identifier, usize),
+        length: usize,
+        component_map: &HashMap<TypeId, usize, FnvBuildHasher>,
+    ) -> <Self::Result as Iterator>::Item {
+        // SAFETY: `columns` is guaranteed to contain raw parts for a valid `Vec<C>` of size
+        // `length`. Since `component_map` contains an entry for the given component `C`'s entry in
+        // `columns`, then the column obtained here can be interpreted as a slice of type `C` of
+        // size `length`. `index` is guaranteed to be a valid index into the `Vec<C>`.
+        unsafe {
+            slice::from_raw_parts_mut::<'a, C>(
+                columns
+                    .get_unchecked(*component_map.get(&TypeId::of::<C>()).unwrap_unchecked())
+                    .0
+                    .cast::<C>(),
+                length,
+            )
+            .get_unchecked_mut(index)
+        }
     }
 
     fn assert_claim(buffer: &mut AssertionBuffer) {
@@ -139,6 +210,32 @@ where
         }
     }
 
+    unsafe fn view_one(
+        index: usize,
+        columns: &[(*mut u8, usize)],
+        _entity_identifiers: (*mut entity::Identifier, usize),
+        length: usize,
+        component_map: &HashMap<TypeId, usize, FnvBuildHasher>,
+    ) -> <Self::Result as Iterator>::Item {
+        match component_map.get(&TypeId::of::<C>()) {
+            Some(component_index) => Some(
+                // SAFETY: `columns` is guaranteed to contain raw parts for a valid `Vec<C>` of
+                // size `length`. Since `component_map` contains an entry for the given component
+                // `C`'s entry in `columns`, then the column obtained here can be interpreted as a
+                // slice of type `C` of size `length`. `index` is guaranteed to be a valid index
+                // into the `Vec<C>`.
+                unsafe {
+                    slice::from_raw_parts(
+                        columns.get_unchecked(*component_index).0.cast::<C>(),
+                        length,
+                    )
+                    .get_unchecked(index)
+                },
+            ),
+            None => None,
+        }
+    }
+
     fn assert_claim(buffer: &mut AssertionBuffer) {
         buffer.claim_immutable::<C>();
     }
@@ -179,6 +276,32 @@ where
         }
     }
 
+    unsafe fn view_one(
+        index: usize,
+        columns: &[(*mut u8, usize)],
+        _entity_identifiers: (*mut entity::Identifier, usize),
+        length: usize,
+        component_map: &HashMap<TypeId, usize, FnvBuildHasher>,
+    ) -> <Self::Result as Iterator>::Item {
+        match component_map.get(&TypeId::of::<C>()) {
+            Some(component_index) => Some(
+                // SAFETY: `columns` is guaranteed to contain raw parts for a valid `Vec<C>` of
+                // size `length`. Since `component_map` contains an entry for the given component
+                // `C`'s entry in `columns`, then the column obtained here can be interpreted as a
+                // slice of type `C` of size `length`. `index` is guaranteed to be a valid index
+                // into the `Vec<C>`.
+                unsafe {
+                    slice::from_raw_parts_mut(
+                        columns.get_unchecked(*component_index).0.cast::<C>(),
+                        length,
+                    )
+                    .get_unchecked_mut(index)
+                },
+            ),
+            None => None,
+        }
+    }
+
     fn assert_claim(buffer: &mut AssertionBuffer) {
         buffer.claim_mutable::<C>();
     }
@@ -198,6 +321,21 @@ impl<'a> ViewSeal<'a> for entity::Identifier {
         unsafe { slice::from_raw_parts_mut::<'a, Self>(entity_identifiers.0, length) }
             .iter()
             .copied()
+    }
+
+    unsafe fn view_one(
+        index: usize,
+        _columns: &[(*mut u8, usize)],
+        entity_identifiers: (*mut entity::Identifier, usize),
+        length: usize,
+        _component_map: &HashMap<TypeId, usize, FnvBuildHasher>,
+    ) -> <Self::Result as Iterator>::Item {
+        // SAFETY: `entity_identifiers` is guaranteed to contain the raw parts for a valid
+        // `Vec<entity::Identifier>` of size `length`. `index` is guaranteed to be a valid index
+        // into the `Vec<entity::Identifier>`.
+        *unsafe { slice::from_raw_parts_mut::<'a, Self>(entity_identifiers.0, length) 
+            .get_unchecked(index)
+        }
     }
 
     fn assert_claim(_buffer: &mut AssertionBuffer) {}
@@ -225,6 +363,31 @@ pub trait ViewsSeal<'a>: Claim {
         component_map: &HashMap<TypeId, usize, FnvBuildHasher>,
     ) -> Self::Results;
 
+    /// View a specific row of an `Archetype`.
+    /// 
+    /// # Safety
+    /// Each tuple in `columns` must contain the raw parts for a valid `Vec<C>` of size `length`
+    /// for components `C`. Each of those components `C` must have an entry in `component_map`,
+    /// paired with the correct index corresponding to that component's entry in `columns`.
+    ///
+    /// `entity_identifiers` must contain the raw parts for a valid `Vec<entity::Identifier` of
+    /// size `length`.
+    ///
+    /// `component_map` must contain an entry for every component `C` that is viewed by this
+    /// `View`, and that entry must contain the index for the column of type `C` in `columns`. Note
+    /// that it is not required for optionally viewed components to be contained in the
+    /// `component_map`.
+    /// 
+    /// `index` must be a valid index in the `Vec<entity::Identifier>` and each component column
+    /// `Vec<C>`.
+    unsafe fn view_one(
+        index: usize,
+        columns: &[(*mut u8, usize)],
+        entity_identifiers: (*mut entity::Identifier, usize),
+        length: usize,
+        component_map: &HashMap<TypeId, usize, FnvBuildHasher>,
+    ) -> <Self::Results as Iterator>::Item;
+
     fn assert_claims(buffer: &mut AssertionBuffer);
 }
 
@@ -238,6 +401,16 @@ impl<'a> ViewsSeal<'a> for Null {
         _component_map: &HashMap<TypeId, usize, FnvBuildHasher>,
     ) -> Self::Results {
         iter::repeat(result::Null)
+    }
+
+    unsafe fn view_one(
+        _index: usize,
+        _columns: &[(*mut u8, usize)],
+        _entity_identifiers: (*mut entity::Identifier, usize),
+        _length: usize,
+        _component_map: &HashMap<TypeId, usize, FnvBuildHasher>,
+    ) -> <Self::Results as Iterator>::Item {
+        result::Null
     }
 
     fn assert_claims(_buffer: &mut AssertionBuffer) {}
@@ -265,6 +438,23 @@ where
                 length,
                 component_map,
             ))
+        }
+    }
+
+    unsafe fn view_one(
+        index: usize,
+        columns: &[(*mut u8, usize)],
+        entity_identifiers: (*mut entity::Identifier, usize),
+        length: usize,
+        component_map: &HashMap<TypeId, usize, FnvBuildHasher>,
+    ) -> <Self::Results as Iterator>::Item {
+        // SAFETY: The safety guarantees of this method are the exact what are required by the
+        // safety guarantees of both `V::view_one()` and `W::view_one()`.
+        unsafe {
+            (
+                V::view_one(index, columns, entity_identifiers, length, component_map),
+                W::view_one(index, columns, entity_identifiers, length, component_map),
+            )
         }
     }
 
