5c4cf24ae073a0ac34f5b6de281f85145e993be8
Anders429 <anders.stefan.evensen@gmail.com>
2022-09-11 23:12:34 -0700

    Fix stages trybuild test.

--- tests/trybuild/stages/system_missing_comma.rs
+++ tests/trybuild/stages/system_missing_comma.rs
@@ -6,10 +6,10 @@ impl<'a> System<'a> for MySystem {
     type Views = views!();
     type Filter = filter::None;
 
-    fn run<R, FI, VI, P, I>(&mut self, query_results: result::Iter<'a, R, Self::Filter, FI, Self::Views, VI>)
+    fn run<R, FI, VI, P, I, Q>(&mut self, query_results: result::Iter<'a, R, Self::Filter, FI, Self::Views, VI, P, I, Q>)
     where
         R: Registry + 'a,
-        R::Viewable: ContainsViews<'a, Self::Views, P, I>,
+        R::Viewable: ContainsViews<'a, Self::Views, P, I, Q>,
         Self::Filter: Filter<R, FI>,
         Self::Views: Filter<R, VI>, {}
 }
