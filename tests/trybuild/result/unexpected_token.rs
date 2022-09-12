2b110ce4ae98ababe9a016abf7512c20fd2dc24f
Anders429 <anders.stefan.evensen@gmail.com>
2022-09-11 23:14:30 -0700

    Replace result::Null with view::Null in trybuild registry tests.

--- tests/trybuild/result/unexpected_token.rs
+++ tests/trybuild/result/unexpected_token.rs
@@ -1,9 +1,9 @@
-use brood::query::result;
+use brood::query::{result, view};
 
 // Define components.
 struct A;
 struct B;
 
 fn main() {
-    let result!(a, + b) = (A, (A, result::Null));
+    let result!(a, + b) = (A, (A, view::Null));
 }
