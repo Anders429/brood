93fc4a5648f869a04e51b1f06d7992b54a42b350
Anders429 <anders.stefan.evensen@gmail.com>
2023-02-08 00:54:37 -0800

    Length tests.

--- src/resource/length.rs
+++ src/resource/length.rs
@@ -14,3 +14,24 @@ where
 {
     const LEN: usize = Resources::LEN + 1;
 }
+
+#[cfg(test)]
+mod tests {
+    use super::Length;
+    use crate::Resources;
+
+    #[test]
+    fn empty_length() {
+        assert_eq!(<Resources!()>::LEN, 0);
+    }
+
+    #[test]
+    fn single_length() {
+        assert_eq!(<Resources!(u8)>::LEN, 1);
+    }
+
+    #[test]
+    fn multiple_length() {
+        assert_eq!(<Resources!(u8, u16, u32, u64)>::LEN, 4);
+    }
+}
