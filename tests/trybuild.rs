2a384d9a739183a54ad88bc15f265fdc311b8400
Anders429 <anders.stefan.evensen@gmail.com>
2023-05-31 14:51:28 -0700

    Switch to stable MSRV channel for trybuild tests.

--- tests/trybuild.rs
+++ tests/trybuild.rs
@@ -1,8 +1,16 @@
 #![cfg(not(skip_trybuild))]
 
+#[test]
+fn check_msrv() {
+    // If this test fails, the MSRV needs to be updated both here and in the `trybuild_test!` macro
+    // definition. This ensures that the trybuild tests are run on the MSRV even when the MSRV is
+    // updated.
+    assert_eq!(env!("CARGO_PKG_RUST_VERSION"), "1.65.0")
+}
+
 macro_rules! trybuild_test {
     ($test_name:ident) => {
-        #[rustversion::attr(not(nightly), ignore)]
+        #[rustversion::attr(not(stable(1.65)), ignore)]
         #[test]
         fn $test_name() {
             trybuild::TestCases::new().compile_fail(concat!(
